e2c305ef211636127cc71df1b5cf7b14268b484f
imcheong <imcheong001@gmail.com>
2020-06-04 22:30:23 +0900

    뿌리는 방식 고민중..

--- src/container/Showtime.js
+++ src/container/Showtime.js
@@ -8,58 +8,39 @@ class Showtime extends Component{
             hour : "",
             minutes : ""
         } 
-       
-    }
 
-    // componentWillUpdate (){
-    //     let date = new Date();
-    //     this.setState({
-    //         hour : date.getHours(),
-    //         minutes : date.getMinutes()
-    //     })
-
-    //     if(this.state.minutes > 45) {
-    //             this.setState({       
-    //             hour : this.state.hour + 1
-    //         }) 
-    //     }
-
-    // }
-
-
-   /* componentWillUpdate (){
-        
         let date = new Date();
-        this.setState({
+        this.state = {
             hour : date.getHours(),
             minutes : date.getMinutes()
-        })
+        }
+        console.log(this.state);
+    }
 
-
+    componentWillUpdate(){
         if(this.state.minutes > 45) {
                 this.setState({       
                 hour : this.state.hour + 1
             }) 
         }
+        var showTime;
+        showTime = this.props.busList.find(bus => (bus.time_hour >= this.state.hour && bus.time_min > this.state.minutes));
         
-        const showTime = this.props.busList.find(bus => (bus.time_hour >= this.state.hour && bus.time_min > this.state.minutes));
-        
-        console.log(showTime);
+
+        for( var key in showTime ) {
+            console.log( key + '=>' + showTime[key] );
+            
+        }
     }
-*/
+    
 
     render(){ 
-        let date = new Date();
-        console.log(date);
-        console.log(date.getHours());
-        console.log(date.getMinutes());
-        console.log(this.props.busList);
-        const view = date[0];
-        console.log(view);
+        
         return (
             <div>
-            <h1>{view}</h1>
-            
+                <h1>
+                    
+                </h1>
             </div>
         ) 
     } 
